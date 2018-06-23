deac1f2cddcd7b5b28cee9e70859bc67d16ed01c
gilkercj <charlesjgilker@gmail.com>
2018-06-22 22:40:19 -0400

    various changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
+import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
@@ -10,8 +10,8 @@ const App = () => (
     <div>
       <Nav />
       <Switch>
-        <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Home" component={Home} />
         <Route exact path="/books/:id" component={Detail} />
         <Route component={NoMatch} />
       </Switch>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,53 @@
+// import axios from "axios";
+
+// export default {
+//   // Gets all books
+//   getBooks: function() {
+//     return axios.get("/api/books");
+//   },
+//   // Gets the book with the given id
+//   getBook: function(id) {
+//     return axios.get("/api/books/" + id);
+//   },
+//   // Deletes the book with the given id
+//   deleteBook: function(id) {
+//     return axios.delete("/api/books/" + id);
+//   },
+//   // Saves a book to the database
+//   saveBook: function(bookData) {
+//     return axios.post("/api/books", bookData);
+//   }
+// };
+
 import axios from "axios";
 
+const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
+const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+
+// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  search: function(query) {
+    return axios.get(BASEURL + query + APIKEY)
+    .done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
+  },
+    getArticles: function() {
+    return axios.get("/api/articles");
   },
   // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  getArticles: function(id) {
+    return axios.get("/api/articles/" + id);
   },
   // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  deleteArticles: function(id) {
+    return axios.delete("/api/articles/" + id);
   },
   // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  saveArticles: function(articleData) {
+    return axios.post("/api/articles", articleData);
   }
 };
+
